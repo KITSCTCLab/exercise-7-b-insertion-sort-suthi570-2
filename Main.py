a0101ec93ed820f2849c25027a606cb9df32b61c
suthi570 <109793462+suthi570@users.noreply.github.com>
2022-11-03 22:01:16 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,13 @@
 from typing import List
+def selectionSort(array, size) -> List[int]:
+  for step in range(size):
+    min_idx = step
+    for i in range(step + 1, size):
+      if array[i] < array[min_idx]:
+        min_idx = i
+    (array[step], array[min_idx]) = (array[min_idx], array[step])
+  return array
 
-def insertionSort(array) -> List[int]:
-  # Write your code here
-
-# data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -11,4 +15,4 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(insertionSort(data))
+print(selectionSort(data, len(data)))
